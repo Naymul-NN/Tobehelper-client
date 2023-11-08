2ed127bb195c20f9a5ae7396f504ea2397b85dc0
Naymul-NN <naymulislam108@gmail.com>
2023-11-08 08:38:42 +0600

    .env file implement

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjTmCdTJCb9oa3cNI5rPpoS6a6AaCr5q0",
-  authDomain: "tobehelper-auth.firebaseapp.com",
-  projectId: "tobehelper-auth",
-  storageBucket: "tobehelper-auth.appspot.com",
-  messagingSenderId: "275813186032",
-  appId: "1:275813186032:web:5cacfa9c93385b535a0535"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
